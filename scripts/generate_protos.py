eafba1d39a60654632fb448129f12d59da0dce75
caleb-imaige <caleb@imaige.com>
2023-12-18 11:53:27 -0700

    sharpened documentation & type hinting for generate_protos & photo_letariat

--- scripts/generate_protos.py
+++ scripts/generate_protos.py
@@ -3,7 +3,7 @@ import subprocess
 import photo_letariat
 
 
-def generate_proto_and_replace_import(proto_directory, files_out_location, proto_file):
+def generate_protos_and_replace_import(proto_directory, files_out_location, proto_file) -> None:
     # enforcement that protocol buffer generation will happen successfully
     # proto_file must end with .proto
     assert (proto_file[-6:]) == '.proto', "The provided proto_file must be of file type .proto"
@@ -15,6 +15,8 @@ def generate_proto_and_replace_import(proto_directory, files_out_location, proto
         assert proto_dir_path[i] == proto_file_path[i], ("The file path for the proto_directory must match that of the "
                                                          "proto_file")
 
+    # requiring files_out_location to be the end location for all files (_pb2.py, _pb2.pyi, and _pb2_grpc.py) ensures
+    # import statements between said files are standard & in turn, that photo_letariat generates correct imports
     command1 = [
         "python3",
         "-m",
@@ -30,7 +32,7 @@ def generate_proto_and_replace_import(proto_directory, files_out_location, proto
 
     proto_buffer_file_name = photo_letariat.get_protocol_buffer_file_name(proto_file)
     proto_buffer_file_path = f"{files_out_location}/{proto_buffer_file_name}_pb2_grpc.py"
-    print(f"replacing text in {proto_buffer_file_path}")
+
     photo_letariat.replace_pb2_import_statement(proto_buffer_file_path, proto_buffer_file_name+"_pb2")
 
 
@@ -43,4 +45,4 @@ if __name__ == "__main__":
     files_out_arg = sys.argv[2]
     proto_file_arg = sys.argv[3]
 
-    generate_proto_and_replace_import(proto_directory_arg, files_out_arg, proto_file_arg)
+    generate_protos_and_replace_import(proto_directory_arg, files_out_arg, proto_file_arg)
