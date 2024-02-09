aa1628e0c5f43c31a4235f512dd5eb3d61525d54
caleb-imaige <caleb@imaige.com>
2024-02-09 09:07:32 -0700

    cleanup of docker image generation workflow

--- scripts/refresh_tls_certs_on_k8s.sh
+++ scripts/refresh_tls_certs_on_k8s.sh
@@ -2,4 +2,5 @@ kubectl delete secret tls-certs
 kubectl create secret generic tls-certs --from-file=./server-key.pem --from-file=./server-cert.pem --from-file=ca-cert.pem --from-file=client-key.pem --from-file=client-cert.pem
 
 # TODO future:
-# 1) update file pathing to be smarter - either take input, or know whether to run either on current dir or on ../tls_certs/k8s
+# 1) take name of secret as input
+# 2) update file pathing to be smarter - either take input, or know whether to run either on current dir or on ../tls_certs/k8s
