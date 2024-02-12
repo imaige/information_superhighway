efa7b212a402d3b0cca0697c8feab504b9a35025
caleb-imaige <caleb@imaige.com>
2024-02-12 10:58:39 -0700

    updated script to accept user input for secret name and path, and to delete & recreate secrets if one by input name already exists

--- scripts/refresh_tls_certs_on_k8s.sh
+++ scripts/refresh_tls_certs_on_k8s.sh
@@ -1,6 +1,24 @@
-kubectl delete secret tls-certs
-kubectl create secret generic tls-certs --from-file=./server-key.pem --from-file=./server-cert.pem --from-file=ca-cert.pem --from-file=client-key.pem --from-file=client-cert.pem
+# refresh_tls_certs_on_k8s.sh
+# requirements: installed AWS CLI & successful authentication - includes:
+# a) AWS user registration to ConfigMap in EKS, and
+# b) registry auth, see 'Authenticate to your default registry' here:
+# https://docs.aws.amazon.com/AmazonECR/latest/userguide/getting-started-cli.html
 
-# TODO future:
-# 1) take name of secret as input
-# 2) update file pathing to be smarter - either take input, or know whether to run either on current dir or on ../tls_certs/k8s
+# Get user input for secret_name
+read -p "Enter the name of the secret: " secret_name
+
+# Get user input for the relative path to the folder containing certificates
+read -p "Enter the relative path to the folder containing certificates: " cert_folder
+
+# Check if the secret exists
+if kubectl get secret "$secret_name" &> /dev/null; then
+    # If the secret exists, delete it
+    echo "Deleting existing secret $secret_name..."
+    kubectl delete secret "$secret_name"
+fi
+
+#kubectl delete secret tls-certs
+
+# Create the new secret
+echo "Creating new secret named $secret_name..."
+kubectl create secret generic "$secret_name" --from-file="$cert_folder/server-key.pem" --from-file="$cert_folder/server-cert.pem" --from-file="$cert_folder/ca-cert.pem" --from-file="$cert_folder/client-key.pem" --from-file="$cert_folder/client-cert.pem"
