bb72d3ba146823c8f2431b774c87cfb47bab3df5
caleb-imaige <caleb@imaige.com>
2024-06-25 12:34:24 -0600

    fixed typo in column name for classifications model

--- proto_models/analysis_layer_pb2.pyi
+++ proto_models/analysis_layer_pb2.pyi
@@ -6,7 +6,7 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Op
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class AiModelOutputRequest(_message.Message):
-    __slots__ = ("photo_id", "image_comparison_run_id", "image_comparison_name", "image_comparison_datatype", "image_comparison_shape", "average_hash", "perceptual_hash", "difference_hash", "wavelet_hash_haar", "color_hash", "color_averages", "bounding_boxes_from_faces_model", "labels_from_classification_model")
+    __slots__ = ("photo_id", "image_comparison_run_id", "image_comparison_name", "image_comparison_datatype", "image_comparison_shape", "average_hash", "perceptual_hash", "difference_hash", "wavelet_hash_haar", "color_hash", "color_averages", "bounding_boxes_from_faces_model", "labels_from_classifications_model")
     PHOTO_ID_FIELD_NUMBER: _ClassVar[int]
     IMAGE_COMPARISON_RUN_ID_FIELD_NUMBER: _ClassVar[int]
     IMAGE_COMPARISON_NAME_FIELD_NUMBER: _ClassVar[int]
@@ -19,7 +19,7 @@ class AiModelOutputRequest(_message.Message):
     COLOR_HASH_FIELD_NUMBER: _ClassVar[int]
     COLOR_AVERAGES_FIELD_NUMBER: _ClassVar[int]
     BOUNDING_BOXES_FROM_FACES_MODEL_FIELD_NUMBER: _ClassVar[int]
-    LABELS_FROM_CLASSIFICATION_MODEL_FIELD_NUMBER: _ClassVar[int]
+    LABELS_FROM_CLASSIFICATIONS_MODEL_FIELD_NUMBER: _ClassVar[int]
     photo_id: int
     image_comparison_run_id: str
     image_comparison_name: str
@@ -32,8 +32,8 @@ class AiModelOutputRequest(_message.Message):
     color_hash: bytes
     color_averages: str
     bounding_boxes_from_faces_model: int
-    labels_from_classification_model: _containers.RepeatedScalarFieldContainer[bytes]
-    def __init__(self, photo_id: _Optional[int] = ..., image_comparison_run_id: _Optional[str] = ..., image_comparison_name: _Optional[str] = ..., image_comparison_datatype: _Optional[str] = ..., image_comparison_shape: _Optional[int] = ..., average_hash: _Optional[bytes] = ..., perceptual_hash: _Optional[bytes] = ..., difference_hash: _Optional[bytes] = ..., wavelet_hash_haar: _Optional[bytes] = ..., color_hash: _Optional[bytes] = ..., color_averages: _Optional[str] = ..., bounding_boxes_from_faces_model: _Optional[int] = ..., labels_from_classification_model: _Optional[_Iterable[bytes]] = ...) -> None: ...
+    labels_from_classifications_model: _containers.RepeatedScalarFieldContainer[bytes]
+    def __init__(self, photo_id: _Optional[int] = ..., image_comparison_run_id: _Optional[str] = ..., image_comparison_name: _Optional[str] = ..., image_comparison_datatype: _Optional[str] = ..., image_comparison_shape: _Optional[int] = ..., average_hash: _Optional[bytes] = ..., perceptual_hash: _Optional[bytes] = ..., difference_hash: _Optional[bytes] = ..., wavelet_hash_haar: _Optional[bytes] = ..., color_hash: _Optional[bytes] = ..., color_averages: _Optional[str] = ..., bounding_boxes_from_faces_model: _Optional[int] = ..., labels_from_classifications_model: _Optional[_Iterable[bytes]] = ...) -> None: ...
 
 class StatusReply(_message.Message):
     __slots__ = ("photo_id",)
