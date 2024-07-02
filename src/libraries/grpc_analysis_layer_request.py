f5926c4d7c68b8828f9c53726bcf4e8db08c09d1
caleb-imaige <caleb@imaige.com>
2024-07-02 10:12:44 -0600

    cleaner interceptor implementation

--- src/libraries/grpc_analysis_layer_request.py
+++ src/libraries/grpc_analysis_layer_request.py
@@ -31,16 +31,15 @@ logger = logging.getLogger(__name__)
 configure_logger(logger, level=logging.INFO)
 
 
-class LoggingClientInterceptor(UnaryUnaryClientInterceptor, UnaryStreamClientInterceptor,
-                               StreamUnaryClientInterceptor, StreamStreamClientInterceptor):
+class LoggingClientInterceptor(UnaryStreamClientInterceptor):
     async def intercept_unary_stream(self, continuation, client_call_details, request):
         logger.info("intercept_unary_stream called")
-        method = client_call_details.method
-        timeout = client_call_details.timeout
-        metadata = client_call_details.metadata
-        logger.info(f"Request Method: {method}")
-        logger.info(f"Request Timeout: {timeout}")
-        logger.info(f"Request Metadata: {metadata}")
+        # method = client_call_details.method
+        # timeout = client_call_details.timeout
+        # metadata = client_call_details.metadata
+        # logger.info(f"Request Method: {method}")
+        # logger.info(f"Request Timeout: {timeout}")
+        # logger.info(f"Request Metadata: {metadata}")
         logger.info(f"Request: {request}")
 
         call = await continuation(client_call_details, request)
@@ -53,41 +52,41 @@ class LoggingClientInterceptor(UnaryUnaryClientInterceptor, UnaryStreamClientInt
         return call
 
     # The next 3 methods should not get used (as we are using a unary_stream channel)
-    async def intercept_unary_unary(self, continuation, client_call_details, request):
-        logger.info("interecept_unary_unary called")
-        self.log_request(client_call_details, request)
-        response = await continuation(client_call_details, request)
-        self.log_response(response)
-        return response
-
-    async def intercept_stream_unary(self, continuation, client_call_details, request_iterator):
-        logger.info("interecept_stream_unary called")
-        self.log_request(client_call_details, request_iterator)
-        response = await continuation(client_call_details, request_iterator)
-        self.log_response(response)
-        return response
-
-    async def intercept_stream_stream(self, continuation, client_call_details, request_iterator):
-        logger.info("interecept_stream_stream called")
-        self.log_request(client_call_details, request_iterator)
-        response = await continuation(client_call_details, request_iterator)
-        async for resp in response:
-            self.log_response(resp)
-        return response
-
-    def log_request(self, client_call_details, request):
-        logger.info("log_request called")
-        method = client_call_details.method
-        timeout = client_call_details.timeout
-        metadata = client_call_details.metadata
-        logger.info(f"Request Method: {method}")
-        logger.info(f"Request Timeout: {timeout}")
-        logger.info(f"Request Metadata: {metadata}")
-        logger.info(f"Request: {request}")
-
-    def log_response(self, response):
-        logger.info("log_response called")
-        logger.info(f"Response: {response}")
+    # async def intercept_unary_unary(self, continuation, client_call_details, request):
+    #     logger.info("interecept_unary_unary called")
+    #     self.log_request(client_call_details, request)
+    #     response = await continuation(client_call_details, request)
+    #     self.log_response(response)
+    #     return response
+    #
+    # async def intercept_stream_unary(self, continuation, client_call_details, request_iterator):
+    #     logger.info("interecept_stream_unary called")
+    #     self.log_request(client_call_details, request_iterator)
+    #     response = await continuation(client_call_details, request_iterator)
+    #     self.log_response(response)
+    #     return response
+    #
+    # async def intercept_stream_stream(self, continuation, client_call_details, request_iterator):
+    #     logger.info("interecept_stream_stream called")
+    #     self.log_request(client_call_details, request_iterator)
+    #     response = await continuation(client_call_details, request_iterator)
+    #     async for resp in response:
+    #         self.log_response(resp)
+    #     return response
+    #
+    # def log_request(self, client_call_details, request):
+    #     logger.info("log_request called")
+    #     method = client_call_details.method
+    #     timeout = client_call_details.timeout
+    #     metadata = client_call_details.metadata
+    #     logger.info(f"Request Method: {method}")
+    #     logger.info(f"Request Timeout: {timeout}")
+    #     logger.info(f"Request Metadata: {metadata}")
+    #     logger.info(f"Request: {request}")
+    #
+    # def log_response(self, response):
+    #     logger.info("log_response called")
+    #     logger.info(f"Response: {response}")
 
 
 async def analysis_layer_request(req: AiModelOutputRequest, port: str, request_location: str = None) -> None:
