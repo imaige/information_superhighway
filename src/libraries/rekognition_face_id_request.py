eb659f4f1ae7f588a52790579d1876bd85d2fe2d
caleb-imaige <caleb@imaige.com>
2024-07-30 10:07:45 -0600

    added logging to rekognition library

--- src/libraries/rekognition_face_id_request.py
+++ src/libraries/rekognition_face_id_request.py
@@ -1,34 +1,45 @@
 import boto3
 import base64
 
+from src.libraries.logging_file_format import configure_logger
+import logging
+
+
+logger = logging.getLogger(__name__)
+configure_logger(logger, level=logging.INFO)
+
 rekognition_client = boto3.client('rekognition')
 
 
 def analyze_face(b64image: str):
+    logger.info("starting analyze_face")
     # decode the base64 string to bytes for rekognition
     image_bytes = base64.b64decode(b64image)
 
+    logger.info("making request in analyze_face")
     response = rekognition_client.detect_faces(
         Image={'Bytes': image_bytes},
         Attributes=['ALL']
     )
 
+    logger.info(f"response in analyze_face is: {response}")
+
     for faceDetail in response['FaceDetails']:
-        print(f"Confidence: {faceDetail['Confidence']:.2f}%")
-        print("BoundingBox:")
-        print(f"  Top: {faceDetail['BoundingBox']['Top']:.2f}")
-        print(f"  Left: {faceDetail['BoundingBox']['Left']:.2f}")
-        print(f"  Width: {faceDetail['BoundingBox']['Width']:.2f}")
-        print(f"  Height: {faceDetail['BoundingBox']['Height']:.2f}")
+        logger.info(f"Confidence: {faceDetail['Confidence']:.2f}%")
+        logger.info("BoundingBox:")
+        logger.info(f"  Top: {faceDetail['BoundingBox']['Top']:.2f}")
+        logger.info(f"  Left: {faceDetail['BoundingBox']['Left']:.2f}")
+        logger.info(f"  Width: {faceDetail['BoundingBox']['Width']:.2f}")
+        logger.info(f"  Height: {faceDetail['BoundingBox']['Height']:.2f}")
 
-        print("Emotions:")
+        logger.info("Emotions:")
         for emotion in faceDetail['Emotions']:
-            print(f"  Type: {emotion['Type']}, Confidence: {emotion['Confidence']:.2f}%")
+            logger.info(f"  Type: {emotion['Type']}, Confidence: {emotion['Confidence']:.2f}%")
 
-        print("Gender:")
-        print(f"  Value: {faceDetail['Gender']['Value']}, Confidence: {faceDetail['Gender']['Confidence']:.2f}%")
+        logger.info("Gender:")
+        logger.info(f"  Value: {faceDetail['Gender']['Value']}, Confidence: {faceDetail['Gender']['Confidence']:.2f}%")
 
-        print("Age Range:")
-        print(f"  Low: {faceDetail['AgeRange']['Low']}, High: {faceDetail['AgeRange']['High']}")
+        logger.info("Age Range:")
+        logger.info(f"  Low: {faceDetail['AgeRange']['Low']}, High: {faceDetail['AgeRange']['High']}")
 
     return response
