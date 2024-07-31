17e82765cc2a9a4d9205fad598a90b3cad13bfef
caleb-imaige <caleb@imaige.com>
2024-07-31 10:27:46 -0600

    updated protos & attempt at correct output structure for gRPC

--- src/libraries/rekognition_face_id_request.py
+++ src/libraries/rekognition_face_id_request.py
@@ -101,6 +101,10 @@ def analyze_face(b64image: str):
             })
             parsed_data_output.append(parsed_data)
             '''
-        parsed_data_output.append({"number_of_faces": number_of_faces})
-        parsed_data_output.append({"bounding_boxes_from_faces_model": json.dumps(bounding_boxes)})
-        return parsed_data_output
+        # parsed_data_output.append({"number_of_faces": number_of_faces})
+        # parsed_data_output.append({"bounding_boxes_from_faces_model": json.dumps(bounding_boxes)})
+        output = {
+            "number_of_faces": number_of_faces,
+            "bounding_boxes_from_faces_model": json.dumps(bounding_boxes)
+        }
+        return output
