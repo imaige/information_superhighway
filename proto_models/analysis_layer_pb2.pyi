e6ec7d64f78e3ab99573e450e5a493f01b103434
caleb-imaige <caleb@imaige.com>
2024-10-23 08:11:17 -0600

    updated protos to link feature extraction

--- proto_models/analysis_layer_pb2.pyi
+++ proto_models/analysis_layer_pb2.pyi
@@ -1,7 +1,7 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -40,16 +40,8 @@ class AiModelOutputRequest(_message.Message):
     number_of_faces: int
     labels_from_classifications_model: _containers.RepeatedScalarFieldContainer[bytes]
     blur_value: float
-    similarity_output: _containers.RepeatedCompositeFieldContainer[FaceExtractOutput]
-    def __init__(self, photo_id: _Optional[str] = ..., project_table_name: _Optional[str] = ..., image_comparison_run_id: _Optional[str] = ..., image_comparison_name: _Optional[str] = ..., image_comparison_datatype: _Optional[str] = ..., image_comparison_shape: _Optional[int] = ..., average_hash: _Optional[bytes] = ..., perceptual_hash: _Optional[bytes] = ..., difference_hash: _Optional[bytes] = ..., wavelet_hash_haar: _Optional[bytes] = ..., color_hash: _Optional[bytes] = ..., color_averages: _Optional[str] = ..., bounding_boxes_from_faces_model: _Optional[str] = ..., number_of_faces: _Optional[int] = ..., labels_from_classifications_model: _Optional[_Iterable[bytes]] = ..., blur_value: _Optional[float] = ..., similarity_output: _Optional[_Iterable[_Union[FaceExtractOutput, _Mapping]]] = ...) -> None: ...
-
-class FaceExtractOutput(_message.Message):
-    __slots__ = ("pixel", "value")
-    PIXEL_FIELD_NUMBER: _ClassVar[int]
-    VALUE_FIELD_NUMBER: _ClassVar[int]
-    pixel: str
-    value: float
-    def __init__(self, pixel: _Optional[str] = ..., value: _Optional[float] = ...) -> None: ...
+    similarity_output: _containers.RepeatedScalarFieldContainer[float]
+    def __init__(self, photo_id: _Optional[str] = ..., project_table_name: _Optional[str] = ..., image_comparison_run_id: _Optional[str] = ..., image_comparison_name: _Optional[str] = ..., image_comparison_datatype: _Optional[str] = ..., image_comparison_shape: _Optional[int] = ..., average_hash: _Optional[bytes] = ..., perceptual_hash: _Optional[bytes] = ..., difference_hash: _Optional[bytes] = ..., wavelet_hash_haar: _Optional[bytes] = ..., color_hash: _Optional[bytes] = ..., color_averages: _Optional[str] = ..., bounding_boxes_from_faces_model: _Optional[str] = ..., number_of_faces: _Optional[int] = ..., labels_from_classifications_model: _Optional[_Iterable[bytes]] = ..., blur_value: _Optional[float] = ..., similarity_output: _Optional[_Iterable[float]] = ...) -> None: ...
 
 class StatusReply(_message.Message):
     __slots__ = ("photo_id", "model_name")
